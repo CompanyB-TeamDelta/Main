e31ea3d8fc59335242fafb3b53add4b7040e94d0
Maksym Diachuk <dyachukmv@gmail.com>
2024-03-09 00:30:57 +0200

    init java app

---
+++ data_processing/src/test/java/ua/edu/ukma/systems_design/data_processing/DataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.systems_design.data_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
