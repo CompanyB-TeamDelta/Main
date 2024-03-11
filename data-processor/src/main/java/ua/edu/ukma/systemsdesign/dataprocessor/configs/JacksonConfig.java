8e64ec8dbcacc71f4552d3307b30235724ab46b7
Maksym Diachuk <dyachukmv@gmail.com>
2024-03-11 23:54:20 +0200

    configure database

--- data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/configs/JacksonConfig.java
+++ data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/configs/JacksonConfig.java
@@ -1,11 +1,6 @@
 package ua.edu.ukma.systemsdesign.dataprocessor.configs;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
