8af6171ff133a4298a4ebefeb011b393d41c3f5a
Maksym Diachuk <dyachukmv@gmail.com>
2024-03-12 17:41:42 +0200

    support monitoring

--- task-scheduler/main.py
+++ task-scheduler/main.py
@@ -12,12 +12,14 @@ db_user = os.environ['DB_USER']
 db_password = os.environ['DB_PASSWORD']
 db_name = os.environ['DB_NAME']
 
-interval = 6
+interval_hours = 6
+monitored_window_days = 7
 
 
 def lambda_handler(event, context):
     current_datetime = datetime.now(timezone.utc)
-    start = current_datetime - timedelta(hours=interval)
+    interval_start = current_datetime - timedelta(hours=interval_hours)
+    monitored_window_start = current_datetime - timedelta(days=monitored_window_days)
 
     conn = pymysql.connect(host=db_host, user=db_user, passwd=db_password, db=db_name, connect_timeout=5)
 
@@ -29,7 +31,7 @@ def lambda_handler(event, context):
 
             message_body = json.dumps({
                 'telegram_channel_id': telegram_channel_id,
-                'from': start.isoformat(),
+                'from': min(interval_start, monitored_window_start).isoformat(),
                 'to': current_datetime.isoformat(),
                 'is_backfill': False
             })
