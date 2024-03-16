38ac68499589a57b15f8f6d7b877f37635976832
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-16 11:58:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "archive_file" "zip_the_python_code" {
 resource "aws_lambda_function" "terraform_lambda_func" {
   filename      = "${path.module}/task-scheduler/scheduler.zip"
   function_name = "shceduler"
-  role          = "arn:aws:iam::***:role/service-role/testFc-role-l1r1aw1v"
+  role          = "arn:aws:iam::531190140983:role/service-role/testFc-role-l1r1aw1v"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
 }
