3e4ae2c741acfd7c5c769b9c39645ce9160a31e1
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-04-14 20:58:55 +0300

    Update NewFilesConsumer.java

--- data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/consumers/NewFilesConsumer.java
+++ data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/consumers/NewFilesConsumer.java
@@ -1,6 +1,11 @@
 package ua.edu.ukma.systemsdesign.dataprocessor.consumers;
 
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,20 +14,26 @@ import ua.edu.ukma.systemsdesign.dataprocessor.models.NewFilesRequest;
 import ua.edu.ukma.systemsdesign.dataprocessor.services.PostUpdatesService;
 
 @Controller
+@RequiredArgsConstructor
+@EnableSqs
+@Slf4j
 public class NewFilesConsumer {
 
     private final PostUpdatesService postUpdatesService;
-
-    @Autowired
-    public NewFilesConsumer(PostUpdatesService postUpdatesService) {
-        this.postUpdatesService = postUpdatesService;
-    }
+    private boolean logged = false;
 
     @SqsListener(
             value = "${aws.sqs.new-files-queue}",
             deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void consumeRequest(@Payload NewFilesRequest request) {
-        System.out.println("Consumed");
-        postUpdatesService.savePostUpdates(request.getTimestamp(), request.getNewFiles());
+    public void consumeRequest( final String input
+            /**
+             * create converter
+             */
+           // @Payload NewFilesRequest request
+    ) {
+        if(!logged) {
+            log.info("[consumeRequest]: " + input);
+            logged = true;
+        }
     }
 }
