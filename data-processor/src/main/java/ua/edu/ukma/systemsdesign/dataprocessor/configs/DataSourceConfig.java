2014b4dc433cd0f9e54ffd73114342aa2701c5a0
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-04-15 08:17:04 +0300

    Update DataSourceConfig.java

--- data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/configs/DataSourceConfig.java
+++ data-processor/src/main/java/ua/edu/ukma/systemsdesign/dataprocessor/configs/DataSourceConfig.java
@@ -5,7 +5,6 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.net.ftp.FTPClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
